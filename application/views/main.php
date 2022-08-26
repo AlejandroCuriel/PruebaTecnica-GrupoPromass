3addda5a3569adc1ba83c4f0039a9f2d01c4be21
Alejandro Curiel <alex_curielg@hotmail.com>
2022-08-25 19:31:27 -0500

    Add base files

---
+++ application/views/main.php
@@ -0,0 +1,26 @@
+<!-- Comienza a escribir tu código aquí -->
+
+
+<!-- Borra el siguiente código: -->
+<div class="py-4rem text-center">
+  <h1 clas>Hola mundo</h1>
+</div>
+<!-- --------------------------- -->
+
+<!-- Hero Banner -->
+<!-- Fin de Hero Banner -->
+
+<!-- 3 Columnas con texto -->
+<!-- Fin de 3 Columnas con texto -->
+
+<!-- Imagen de anchura total con texto encima -->
+<!-- Fin de Imagen de anchura total con texto encima -->
+
+<!-- Imagen de anchura total con texto encima -->
+<!-- Fin de Imagen de anchura total con texto encima -->
+
+<!-- 2 Columnas con texto y círculo Golden -->
+<!-- Fin de 2 Columnas con texto y círculo Golden -->
+
+
+<!-- Termina tu código aquí -->
